18bcc2187e8eefed93fc4277c896be46f9ebfe94
Edgars008 <55804543+Edgars008@users.noreply.github.com>
2019-12-29 18:32:51 +0200

    New upload

---
+++ src/main/java/lv/twino/TwinoApplication.java
@@ -0,0 +1,13 @@
+package lv.twino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/twino/TwinoApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.twino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
